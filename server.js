34104cd0b83a163cab97b5af21af94d6ef4ef0c5
Bhautik <solankibhautik1702@gmail.com>
2025-05-26 15:29:19 +0530

    exported server app

--- server.js
+++ server.js
@@ -66,7 +66,5 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Export for Vercel
+module.exports = app;
